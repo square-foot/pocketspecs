8f40be5fdb46f5edd391be41264d7cbdd5d3aae1
Sabu Francis <sf@sabufrancis.com>
2024-09-04 19:06:57 +0530

    Cleaned patterns/jsToml.pat.js

--- patterns/jsToml.pat.js
+++ patterns/jsToml.pat.js
@@ -1,7 +1,10 @@
-try{
+
     const rnd = $security.randomString(8);
     const tmp = $os.tempDir(); //tmp dir must be available on the server
-    let  inpfile = `${tmp}/${rnd}`;
+    let inpfile = `${tmp}/${rnd}`;
+
+    try{
+
     if(_arg2_ == "-jt"){
         inpfile += '.json';    
     $os.writeFile(inpfile, JSON.stringify(_arg1_), 777);
@@ -9,6 +12,7 @@ try{
         inpfile += '.toml';
         $os.writeFile(inpfile, _arg1_, 777);
     }
+
     //Note cvrt_json_toml.sh has to be in the same folder as the pocketbase executable 
     //and there was no change-dir that was done before invoking this 
     //It internally calls the excellent binary executable for converting json to toml and back
@@ -16,11 +20,12 @@ try{
     
     const cmd = $os.exec('./cvrt_json_toml.sh',_arg2_,inpfile); 
     _arg3_ = String.fromCharCode(...cmd.output());
-    $os.remove(inpfile);
-   
+    
     _arg4_ = true;
     } catch(ee){
         console.log(ee);
         _arg4_  = false;
-       
     }
+    try {
+        $os.remove(inpfile);
+       } catch(ee){;}
